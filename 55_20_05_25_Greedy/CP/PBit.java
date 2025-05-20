e8421668f86236b9598812d81f0d993745777e12
Deepakpottavatri06 <deepakpottavatri06@gmail.com>
2025-05-20 13:21:11 +0530

    programs till 20/05/25

---
+++ 55_20_05_25_Greedy/CP/PBit.java
@@ -0,0 +1,104 @@
+/*A binary word Bn is formed as follows:
+    B[1] = "0"
+    B[i+1] =  B[i] $ "1" $ reverse(complement(B[i])) for i > 1
+
+where $ denotes the concatenation operation, reverse(complement(B)) returns 
+the reversed word of complement(B), which perform 1's complement of B 
+(0 changes to 1 and 1 changes to 0).
+
+For example, the first 4 words in the above sequence are:
+
+    B[1] = "0"
+    B[2] = "011"
+    B[3] = "0111001"
+    B[4] = "011100110110001"
+
+Return the Pth bit in B[N]. It is guaranteed that P is valid for the given N.
+
+Input Format:
+-------------
+Line-1: Two space seperated integers represents N and P.
+
+Output Format:
+--------------
+Return a bit (0 or 1).
+
+
+Sample Input-1:
+---------------
+3 4
+
+Sample Output-1:
+----------------
+1
+
+Explanation:
+------------
+B[3] = "0111001" and 4th bit is 1.
+
+Sample Input-2:
+---------------
+4 10
+
+Sample Output-2:
+----------------
+1
+
+Explanation:
+-------------
+B[4] = "011100110110001" and 10th bit is 1.
+
+*/
+
+import java.util.Scanner;
+
+public class PBit{
+    public static void main (String[] args) {
+        try (Scanner cin = new Scanner(System.in)) {
+            int n = cin.nextInt();
+            int p = cin.nextInt();
+            System.out.println(find(n,p));
+            cin.close();
+        }
+    }
+    static int find(int n, int p){
+        if(n==1){
+            return 0;
+        }
+        
+        int len = (1<<n)-1;
+        int mid = (len/2) + 1;
+        
+        if(mid==p){
+            return 1;
+        }
+        else if(p<mid){
+            return find(n-1,p);
+        }
+        else{
+            int revP = (1<<n-1) - (p-mid);
+            return 1 - find(n-1,revP);
+        }
+    }
+    static char find_Brute(int n, int p){
+        StringBuilder s = new StringBuilder("0");
+        while(n-1>0){
+            StringBuilder t = new StringBuilder(s);
+            t.append("1");
+            t.append(complement(s.reverse().toString()));
+            s.setLength(0);
+            s.append(t.toString());
+            n--;
+        }
+        // System.out.println(s);
+        return s.charAt(p-1);
+    }
+    static String complement(String s){
+        char res[] = s.toCharArray();
+        for(int i = 0; i < s.length(); i++){
+            res[i] = res[i]=='0'? '1': '0';
+        }
+        return new String(res);
+        
+    }
+}
