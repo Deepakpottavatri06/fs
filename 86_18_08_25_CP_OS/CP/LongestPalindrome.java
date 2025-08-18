a3c40c892cbd7b73cc5418bc2554cb2b3b242186
Deepakpottavatri06 <deepakpottavatri06@gmail.com>
2025-08-18 13:33:33 +0530

    OS and CP questions

--- 86_18_08_25_CP_OS/CP/LongestPalindrome.java
+++ 86_18_08_25_CP_OS/CP/LongestPalindrome.java
@@ -1,3 +1,56 @@
+/*Mr Gnanesh is working with words. He has given a list of words. 
+Each word in the list contains only two lowercase letters [a-z].
+
+He wants to create biggest word BW, by concatenating words from the list, which 
+is a palindrome too. He is allowed to use each word from the list atmost once.
+
+Return the length of the biggest word can be formed using the list.
+If it is impossible to create such word, return 0.
+
+Input Format:
+-------------
+Space separated strings, words[], two letter words.
+
+Output Format:
+--------------
+Print an integer result.
+
+
+Sample Input-1:
+---------------
+ab ba dd
+
+Sample Output-1:
+----------------
+6
+
+Explanation: 
+------------
+The biggest word is, ab,dd,ba => abddba, which is a palindrome.
+
+
+Sample Input-2:
+---------------
+pq rs sr mk km pq
+
+Sample Output-2:
+----------------
+8
+
+Explanation: 
+------------
+The biggest word is, rs,sr,mk,km => rsmkkmsr or mkrssrkm..etc, 
+which is a palindrome.
+
+
+Sample Input-3:
+---------------
+aa bb cc
+
+Sample Output-3:
+----------------
+2
+ */
 import java.util.*;
 
 public class LongestPalindrome{
